7933057b847ba9476312c472b4454c3c525bbdad
LethalTedyBear <nferreira1729@gmail.com>
2021-07-07 21:12:38 -0400

    feat: :sparkles: Add heatmap plotting function

--- exploration/dim_reduce.py
+++ exploration/dim_reduce.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from itertools import combinations
+from sklearn.preprocessing import MinMaxScaler
 
 # This script plots all of the attributes of the data set as scatter 
 # plots to determine if any of them depend on each other
@@ -22,9 +24,30 @@ def combination_scatter(df, folder):
 		plt.savefig(f'./{folder}/{label[1]}_vs_{label[0]}.png', dpi=800)
 		plt.clf()
 
+def cov_heatmap(df, title=None, axis_tilt=0):
+	cov = df.cov()	
+	labels = cov.columns
+
+	fig, ax = plt.subplots()
+	im = ax.imshow(cov)
+
+	# Configure axes
+	ax.set_xticks(np.arange(len(labels)))
+	ax.set_yticks(np.arange(len(labels)))
+	# Label Axes
+	ax.set_xticklabels(labels)
+	plt.xticks(rotation=axis_tilt)
+	ax.set_yticklabels(labels)
+	# Create color bar
+	cbar = ax.figure.colorbar(im, ax=ax)
+	# Set title
+	plt.title(title)
+	# Full size figure
+	fig.tight_layout()
+
+	plt.show()	
+
 # Loading data
 df_red_wine = pd.read_csv('../data/winequality-red.csv', delimiter=';')
 df_white_wine = pd.read_csv('../data/winequality-white.csv', delimiter=';')
 
-combination_scatter(df_white_wine.drop(columns=['quality']), 'white_wine_scatter')
-combination_scatter(df_red_wine.drop(columns=['quality']), 'red_wine_scatter')
